fe940e0287fa451f8e7a4e52937397d57263d7de
sodeh <sodeh.tarahomi@yahoo.com>
2019-11-09 14:47:36 -0500

    event

---
+++ Assignment2/Assignment2/Countingarrelements.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Countingarrelements
+    {
+        public void Countingarray(int[] arr1)
+        {
+           
+            bool[] check = new bool[arr1.Length];
+            int count;
+            int j;            
+
+            Array.Sort(arr1);
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (check[i] == true)
+                {
+                    continue;
+                }
+
+                count = 1;
+                for (j = i + 1; j < arr1.Length; j++)
+                {
+                    if (arr1[i] == arr1[j])
+                    {
+                        count++;
+                        check[j] = true;
+                    }
+                    
+                }
+                Console.WriteLine(arr1[i] + "reapeated" + count + "times");
+            }
+
+        }
+    }
+}
+
+
+
+       
---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        private static int K;
+
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[] { 1, 2, 3, 2, 4, 5, 3, 1, 1, 5 };
+            Countingarrelements countelement = new Countingarrelements();
+            countelement.Countingarray(arr1);
+            Console.ReadKey();
+
+        }
+
+    }
+}
